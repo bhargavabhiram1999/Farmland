98da0ae480b2459064014f21f91d53439fb3861e
bhargavabhiram1999 <bhargavabhiram1999@gmail.com>
2025-02-26 02:06:49 +0530

    Updates

---
+++ static/js/952.f1a26353.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfarmland=self.webpackChunkfarmland||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.f1a26353.chunk.js.map
