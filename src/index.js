571872d76ef5902eb166bd3fa9529f4029d56633
bhargavabhiram1999 <bhargavabhiram1999@gmail.com>
2025-02-26 02:15:22 +0530

    as

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* ✅ Wrap App inside BrowserRouter */}
+    <BrowserRouter basename="/farmland"> {/* ✅ Wrap App inside BrowserRouter */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
