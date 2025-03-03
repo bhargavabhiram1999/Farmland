5a0df5ab1b75f663bc8e41c7ce834794c250cd43
bhargavabhiram1999 <bhargavabhiram1999@gmail.com>
2025-03-04 02:46:13 +0530

    Updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import FarmLandNavBar from "./FarmLandNavBar";
-import FetchData from "./FetchData";
 import DisplayMaterialInfo from "./DisplayMaterialInfo";
 
 function Welcome() {
